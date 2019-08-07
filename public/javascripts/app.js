aa951b73df679c7188af693623c032faddbd5a64
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2019-08-07 18:49:54 +0700

    Fix

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
     console.log("show image" + image)
     let lightgallery = $("#lightgallery");
     lightgallery.append(
-      `<li data-source="thumb/${image}/25" full="images/${image}"><a href><img class="placeholder" data-src="thumb/${image}/5" src="img/placeholder.jpg"></a></li>`
+      `<li data-source="thumb/${image}/25" full="images/${image}"><a href><img class="placeholder" data-src="thumb/${image}" src="img/placeholder.jpg"></a></li>`
     );
   }
   function saveToJSON(images) {
--- routes/thumb.js
+++ routes/thumb.js
@@ -4,29 +4,36 @@ const imageThumbnail = require("image-thumbnail");
 const fs = require("fs");
 const c = require("../config");
 
+async function getImage(options, req, res, next) {
+  try {
+    const stream = fs.createReadStream(c.photoDir + "/" + req.params.image);
+    var thumbnail = await imageThumbnail(stream, options);
+
+    res.writeHead(200, {
+      "Content-Type": "image/png",
+      "Content-Length": thumbnail.length
+    });
+    res.end(thumbnail);
+  } catch (err) {
+    next(err);
+  }
+}
+
+router.get("/:image", function (req, res, next) {
+  let options = {
+    width: 300,
+    responseType: "buffer"
+  };
+  getImage(options, req, res, next);
+});
+
 /* GET percentage. */
 router.get("/:image/:percentage", function (req, res, next) {
-    async function getImage() {
-        try {
-          const stream = fs.createReadStream(
-            c.photoDir + "/" + req.params.image
-          );
-          var thumbnail =
-            await imageThumbnail(stream, {
-              percentage: req.query.full ? 100 : req.params.percentage,
-              responseType: 'buffer'
-            });
-
-            res.writeHead(200, {
-              "Content-Type": "image/png",
-              "Content-Length": thumbnail.length
-            });
-            res.end(thumbnail); 
-        } catch (err) {
-          next(err);
-        }
-    }
-    getImage();
+  let options = {
+    percentage: req.query.full ? 100 : req.params.percentage,
+    responseType: "buffer"
+  };
+  getImage(options, req, res, next);
 });
 
 module.exports = router;
