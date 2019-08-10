09dad6a61ff652b920fe193562e07c0775a1dd23
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2019-08-10 23:10:40 +0700

    Add aftermovie

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,61 +2,69 @@ $(document).ready(function() {
   $("#lightgallery").lightGallery();
   let reference = $("#reference");
   let searching = $("#searching");
+
   if (reference.length && searching.length) {
     console.log("searching your face");
-    async function detect() {
-      await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
-      await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
-      await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+    prepareSearching();
+  }
 
-      const referenceResult = await faceapi
-        .detectAllFaces("reference")
+  async function detect(queryResults, referenceResult) {
+    for (let j = 0; j < queryResults.length; j++) {
+      let fd = queryResults[j].descriptor;
+      const distance = await faceapi.euclideanDistance(
+        referenceResult[0].descriptor,
+        fd
+      );
+      if (distance <= 0.45) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  async function prepareSearching() {
+    await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
+    await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+    await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+    const referenceResult = await faceapi
+      .detectAllFaces("reference")
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+
+    if (!referenceResult.length) {
+      return;
+    }
+
+    let images = JSON.parse($("#searching").attr("data"));
+    let found = [];
+    for (let i = 0; i < images.length; i++) {
+      searching.attr("src", "thumb/" + images[i] + "/25");
+      console.log("searching " + images[i]);
+      const queryResults = await faceapi
+        .detectAllFaces("searching")
         .withFaceLandmarks()
         .withFaceDescriptors();
 
-      if (!referenceResult.length) {
-        return;
+      if (await detect(queryResults,referenceResult)) {
+        showImage(images[i]);
+        found.push(images[i]);
       }
-
-      //const faceMatcher = new faceapi.FaceMatcher(referenceResult);
-      let images = JSON.parse($("#searching").attr("data"));
-      let resImages = [];
-      for (let i = 0; i < images.length; i++) {
-        searching.attr("src", "thumb/" + images[i] + "/25");
-        console.log("searching " + images[i]);
-        const queryResults = await faceapi
-          .detectAllFaces("searching")
-          .withFaceLandmarks()
-          .withFaceDescriptors();
-
-        for (let j = 0; j < queryResults.length; j++) {
-          let fd = queryResults[j].descriptor;
-          const distance = faceapi.euclideanDistance(
-            referenceResult[0].descriptor,
-            fd
-          );
-          if (distance <= 0.45) {
-            showImage(images[i]);
-            resImages.push(images[i]);
-            j = queryResults.length;
-          }
-        }
-        $(".meter span:first-child").css(
-          "width",
-          (i / images.length) * 100 + "%"
-        );
-      }
-      saveToJSON(resImages);
+      $(".meter span:first-child").css(
+        "width",
+        (i / images.length) * 100 + "%"
+      );
     }
-    detect();
+    saveToJSON(found);
   }
+
   function showImage(image) {
-    console.log("show image" + image);
+    console.log("show image " + image);
     let lightgallery = $("#lightgallery");
     lightgallery.append(
       `<li data-source="thumb/${image}/25" full="images/${image}"><a href><img src="thumb/${image}"></a></li>`
     );
   }
+
   function saveToJSON(images) {
     $.ajax({
       contentType: "application/json",
@@ -75,7 +83,10 @@ $(document).ready(function() {
       url: "/search/" + $("#reference").attr("data")
     });
   }
+
   var lazyLoadInstance = new LazyLoad({
     elements_selector: ".placeholder"
   });
+  
+  const player = new Plyr("video");
 });
